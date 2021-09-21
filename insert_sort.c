a995a8958bb9f8b901aa496bb2fbdbec27ecc537
MythicAlchemist <thmsmccy@gmail.com>
2021-09-20 22:19:44 -0500

    finished c code for sort

--- insert_sort.c
+++ insert_sort.c
@@ -9,18 +9,23 @@
    that corresponds to our implementation in C.
 */
 
-//  gcc insert_sort.c -o insert_sort.exe -Wall -Werror
+//  gcc insert_sort.c -o insert_sort.exe -Wall -Werror -ggdb
 
 /*
     Shift right: This procedure takes three parameters, the address of an 
     array of ints, the index of the final element in the array, and a position 
     in the array. It overwrites the final element, shifting intermediate 
-    elements to the right, leaving a whole at the position.
+    elements to the right, leaving a hole at the position.
 */
 void shift_right(int *addr, int pos, int final) 
 {
-    
+    int i;
+
+    for (i = final - 1; i >= pos; i--) {
+        addr[i + 1] = addr[i];
+    }
 }
+
 /*
     Find sorted position: This procedure takes three parameters, the address 
     of an array of sorted ints, a value, and the index of the last element in 
@@ -29,10 +34,17 @@ void shift_right(int *addr, int pos, int final)
 */
 int find_sorted_pos(int *addr, int val, int final) 
 {
-    int idx;
+    int i;
 
-    return idx;
+    for (i = 0; i < final; i++) {
+        if (addr[i] >= val) {
+            break;
+        }
+    }
+
+    return i;
 }
+
 /*
     Insert sorted position: This procedure takes three parameters, the address 
     of a partially-sorted array of ints, the index of the first item in the 
@@ -43,8 +55,12 @@ int find_sorted_pos(int *addr, int val, int final)
 */
 void insert_sorted_pos(int *addr, int pos, int final) 
 {
-
+    int v = addr[pos];
+    int p = find_sorted_pos(addr, v, final);
+    shift_right(addr, p, pos);
+    addr[p] = v;
 }
+
 /*
     Insertion sort: This procedure takes two parameters, the address of an 
     array of ints and the number of elements in the array.  It sorts the array 
@@ -52,31 +68,53 @@ void insert_sorted_pos(int *addr, int pos, int final)
 */
 void insertion_sort(int *addr, int length) 
 {
-
+    int i;
+    
+    for (i = 1; i < length; i++) {
+        insert_sorted_pos(addr, i, length - 1);
+    }
 }
+
 /*
     Creates an array at address addr of length elements containing length 
     unique integers in reverse sorted order
 */
 int *fill(int *addr, int length)
 {
+    int i;
 
-
+    for (i = 0; i < length; i++) {
+        addr[i] = length - i;
+    }
+    return addr;
 }
 
 int main(int argc, char* argv[])
 {
-    int *p = 0;
-    int length = 100;
+    int i;
+    int *p;
+    int length;
 
     if(argc >= 2) {
         length = atoi(argv[1]);
     }
 
-    //p = fill(p, length);
-    //insertion_sort(p, length);
+    int addr[length];
+    p = addr;
 
-    printf("%d", length);
+    p = fill(p, length);
+
+    for (i = 0; i < length; i++) {
+        printf("%d ", p[i]);
+    }
+    
+    insertion_sort(p, length);
+
+    // printf("\nLength: %d\n", length);
+    
+    for (i = 0; i < length; i++) {
+        printf("%d ", p[i]);
+    }
 
     return 0;
 }
